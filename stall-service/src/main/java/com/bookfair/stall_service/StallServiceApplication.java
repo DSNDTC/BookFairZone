4c7ecd81724c6292ba28f241d5714591ba8541ad
chathuryaprasad <chathuryaprasad@gmail.com>
2025-10-25 12:15:10 +0530

    Add initial project setup for stall service with Docker, Maven, and Spring Boot configuration

--- stall-service/src/main/java/com/bookfair/stall_service/StallServiceApplication.java
+++ stall-service/src/main/java/com/bookfair/stall_service/StallServiceApplication.java
@@ -3,7 +3,7 @@ package com.bookfair.stall_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.bookfair")
 public class StallServiceApplication {
 
 	public static void main(String[] args) {
