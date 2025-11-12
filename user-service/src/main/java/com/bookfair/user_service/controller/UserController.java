27f23df40686847f883440014db2f7a7fad61d62
nadeeshani00jay@gmail.com <139541836+NadeeshaniJay@users.noreply.github.com>
2025-11-12 18:19:43 +0530

    Added CRUD operations for user management

--- user-service/src/main/java/com/bookfair/user_service/controller/UserController.java
+++ user-service/src/main/java/com/bookfair/user_service/controller/UserController.java
@@ -1,20 +1,65 @@
 package com.bookfair.user_service.controller;
 
-import com.bookfair.user_service.repository.UserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.bookfair.user_service.model.dto.User;
+import com.bookfair.user_service.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-public class UserController {
-    @Autowired
-    UserRepo repo;
+import java.util.List;
 
-    @PostMapping("/addUser")
-    public void addUser(@RequestBody User user) {
-        repo.save(user);
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // Create User
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User newUser = userService.createUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    // Get All Users
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    // Get User by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        if (user != null) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Update User
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails) {
+        User updatedUser = userService.updateUser(id, userDetails);
+        if (updatedUser != null) {
+            return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // Delete User
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- user-service/src/main/java/com/bookfair/user_service/model/dto/User.java
+++ user-service/src/main/java/com/bookfair/user_service/model/dto/User.java
@@ -3,18 +3,71 @@ package com.bookfair.user_service.model.dto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
-@Data
 @Entity
 @Table(name = "users")
 public class User {
-    @Id
-    private Long id;
-    private String name;
-//    private String email;
-//    private String fullName;
-//    private String role;
 
-    // Constructor, getters, setters
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String businessName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+
+    // constructors
+    public User() {
+    }
+
+    public User(String name, String businessName, String email, String phoneNumber, String password) {
+        this.name = name;
+        this.businessName = businessName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+    // getters and setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getBusinessName() {
+        return businessName;
+    }
+    public void setBusinessName(String businessName) {
+        this.businessName = businessName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
