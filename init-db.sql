d1b6ff5179f157b93714c45b7afacceda143c4bf
Malisha4065 <128911037+Malisha4065@users.noreply.github.com>
2025-11-13 08:07:34 +0000

    Integrate api gateway

---
+++ init-db.sql
@@ -0,0 +1,10 @@
+-- Create additional database for security service
+-- This script runs automatically on first PostgreSQL initialization
+
+-- The default database 'reservationdb' is already created via POSTGRES_DB env var
+-- We just need to create the security database
+
+CREATE DATABASE reservation_security;
+
+-- Grant privileges to the main user
+GRANT ALL PRIVILEGES ON DATABASE reservation_security TO myuser;
