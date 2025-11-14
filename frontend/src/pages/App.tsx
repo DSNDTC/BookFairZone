3cc9d570434b80638b9465a9d309eaf3def0dda3
Selani00 <pkselani00@gmail.com>
2025-11-14 09:52:44 +0530

    add stall management UI

---
+++ frontend/src/pages/App.tsx
@@ -0,0 +1,41 @@
+import { Toaster } from "./components/ui/toaster";
+import { Toaster as Sonner } from "./components/ui/sonner";
+import { TooltipProvider } from "./components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import NotFound from "./pages/NotFound";
+import EmployeeLogin from "./pages/EmployeeLogin";
+import EmployeePortal from "./pages/EmployeePortal";
+import Dashboard from "./pages/Dashboard";
+import Genres from "./pages/Genres";
+import Reservations from "./pages/Reservations";
+import StallManagement from "./pages/StallManagement";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/employee-login" element={<EmployeeLogin />} />
+          <Route path="/employee-portal" element={<EmployeePortal />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/genres" element={<Genres />} />
+          <Route path="/reservations" element={<Reservations />} />
+          <Route path="/stall-management" element={<StallManagement />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
