a10cc14e8bdca1aae60c1d9a2e3b9bb86582afad
Selani00 <pkselani00@gmail.com>
2025-11-25 16:00:20 +0530

    update the send email service

--- email-service/src/main/java/com/bookfair/email_service/service/SendEmailService.java
+++ email-service/src/main/java/com/bookfair/email_service/service/SendEmailService.java
@@ -1,26 +1,28 @@
 package com.bookfair.email_service.service;
 
-import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 @Service
 public class SendEmailService {
 
     @Autowired
-    private final JavaMailSender javaMailSender;
+    private JavaMailSender javaMailSender; 
 
     @Value("${spring.mail.username}")
     private String fromEmailId;
     
-    public void SendEmailService(String recipient, String body, String subject) {
-        SimpleMainMessage simpleMailMessage = new SimpleMailMessage();
+    public void sendEmail(String recipient, String body, String subject) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        
         simpleMailMessage.setFrom(fromEmailId);
         simpleMailMessage.setTo(recipient);
         simpleMailMessage.setText(body);
         simpleMailMessage.setSubject(subject);
 
-        
         javaMailSender.send(simpleMailMessage);
     }
-
 }
